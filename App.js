2923e4696330c2ec21bf02c797581b66cd334fc6
twentiecker <twentiecker@gmail.com>
2022-10-05 21:52:59 +0700

    Added Redux

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import configureStore from './src/redux/store';
+
+const store = configureStore();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>My first hello world in react native</Text>
+        <Text>Hi my name is Lukman Eka Arifandhi</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
